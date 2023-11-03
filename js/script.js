7cd4b741d49a3e314f6e39454b9255f490fa0484
Erik Lundell <erik.lundell@sitevision.se>
2023-11-03 08:19:51 +0100

    no dev mode

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const devMode = true;
+const devMode = false;
 let allTheWords, theWord, theWordString, elements, wordBodies, keyboard, points, scoredPoints, noOfAttempts;
 const messageDiv = document.getElementById("message");
 const startView = document.getElementById("startView");
